ea4c1bb0677523e523881ac51be08d116add0309
3lyahmd <alimessibarca23@gmail.com>
2026-05-13 16:41:29 +0300

    Update implementation(User Input).py

---
+++ implementation(User Input).py
@@ -0,0 +1,40 @@
+# (non-recursive)
+def has_triangle(A):
+    A.sort()
+
+    for i in range(len(A) - 2):
+        if A[i] + A[i + 1] > A[i + 2]:
+            return 1
+
+    return 0
+
+#===========================================================
+# (recursive)
+def check_triangle(A, i):
+    if i > len(A) - 3:
+        return 0
+
+    if A[i] + A[i + 1] > A[i + 2]:
+        return 1
+
+    return check_triangle(A, i + 1)
+
+
+def has_triangle_recursive(A):
+    A.sort()
+    return check_triangle(A, 0)
+
+
+#===========================================================
+
+# Data
+user_input = input("Enter numbers separated by spaces: ")
+A = [int(x) for x in user_input.split()]
+#split : هي اللي بتفهم البرنامج ان الارقام منفصلة مش رقم واحد 
+
+# non-recursive Result
+print("non-recursive:", has_triangle(A.copy()))
+
+# Recursive Result
+print("Recursive:", has_triangle_recursive(A.copy()))
+#copy : عشان مغيرش في ال array الاصلية
